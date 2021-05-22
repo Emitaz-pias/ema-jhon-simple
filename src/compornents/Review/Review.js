acabf17e859ba9c9f4c51c15b22403c2cb480cc5
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-05-22 17:14:27 +0600

    product taken from local storage to review components

--- src/compornents/Review/Review.js
+++ src/compornents/Review/Review.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+    const [cart,setCart] = useState([])
+    useEffect(() =>{
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart)
+        const cartProducts = productKeys.map(key=>{
+            const product = fakeData.find(pd=>pd.key ===key)
+            product.quantity = savedCart[key]
+            return product;
+
+        })
+        console.log(cartProducts);
+    },[])
     return (
         <div>
             <h2>this is review</h2>
