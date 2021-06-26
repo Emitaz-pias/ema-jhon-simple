57a3033608cfd2af9338920507d442137cdca0bb
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-26 19:17:38 +0600

    jwt token initailized

--- src/compornents/shipment/Shipment.js
+++ src/compornents/shipment/Shipment.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../App';
+
+
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+     const loggedInUser = useContext(UserContext)
     return (
-        <div>
-            <h1>this is shipment</h1>
-        </div>
-    );
+        <form onSubmit={handleSubmit()}>
+          <input name="name" defaultValue={loggedInUser.displayName} {...register("name")} />
+          {errors.name && <span>Name is required</span>}
+          <input type="submit" />
+        </form>
+      );
 };
 
 export default Shipment;
