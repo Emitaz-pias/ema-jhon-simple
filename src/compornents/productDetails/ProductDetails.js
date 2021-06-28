80ca3d7d90c876780f172c0b963d20d8097eaf87
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-28 14:26:24 +0600

    fake data barried from shop and product details component

--- src/compornents/productDetails/ProductDetails.js
+++ src/compornents/productDetails/ProductDetails.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import fakeData from '../../fakeData';
-import Products from '../Products/Products';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import fakeData from "../../fakeData";
+import Products from "../Products/Products";
 
 const ProductDetails = () => {
-    const { productKey}=useParams();
-    const product = fakeData.find(pd=>pd.key===productKey);
-    return (
-        <div>
-            <h1>YOur product details here</h1> 
-            <Products showAddToCart={false} product={product}></Products>
-        </div>
-    );
+  const { productKey } = useParams();
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8080/product/${productKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, [productKey]);
+  console.log("hi thi si ", product);
+  // const product = detailProduct.find((pd) => pd.key === productKey);
+  return (
+    <div>
+      <h1>YOur product details here</h1>
+      <Products showAddToCart={false} product={product}></Products>
+    </div>
+  );
 };
 
 export default ProductDetails;
