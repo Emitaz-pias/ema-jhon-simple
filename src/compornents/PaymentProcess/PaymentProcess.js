f1aaa22b21e806bbb018c553aace4b6160145955
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-29 17:02:03 +0600

    payment button added

--- src/compornents/PaymentProcess/PaymentProcess.js
+++ src/compornents/PaymentProcess/PaymentProcess.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import CardElementProcessor from "./CardElementProcessor";
 
 const stripePromise = loadStripe("pk_test_iONwyHcRVWakZiYUQOPzcA8d00C5WkKbt2");
 
 const PaymentProcess = () => {
-  return <Elements stripe={stripePromise}></Elements>;
+  return (
+    <Elements stripe={stripePromise}>
+      <CardElementProcessor />
+    </Elements>
+  );
 };
 
 export default PaymentProcess;
--- src/compornents/shipment/Shipment.js
+++ src/compornents/shipment/Shipment.js
@@ -12,88 +12,93 @@ const Shipment = () => {
   } = useForm();
   const loggedInUser = useContext(UserContext);
   return (
-    <div>
-      <form onSubmit={handleSubmit()}>
-        <input
-          name="name"
-          defaultValue={loggedInUser.displayName}
-          {...register("name")}
-        />
-        {errors.name && <span>Name is required</span>}
-        <input type="submit" />
-      </form>
-      <PaymentProcess></PaymentProcess>
-      <div>
-        <form class="row g-3">
-          <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">
-              Email
-            </label>
-            <input type="email" class="form-control" id="inputEmail4" />
-          </div>
-          <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">
-              Password
-            </label>
-            <input type="password" class="form-control" id="inputPassword4" />
-          </div>
-          <div class="col-12">
-            <label for="inputAddress" class="form-label">
-              Address
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress"
-              placeholder="1234 Main St"
-            />
-          </div>
-          <div class="col-12">
-            <label for="inputAddress2" class="form-label">
-              Address 2
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
-            />
-          </div>
-          <div class="col-md-6">
-            <label for="inputCity" class="form-label">
-              City
-            </label>
-            <input type="text" class="form-control" id="inputCity" />
-          </div>
-          <div class="col-md-4">
-            <label for="inputState" class="form-label">
-              State
-            </label>
-            <select id="inputState" class="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div class="col-md-2">
-            <label for="inputZip" class="form-label">
-              Zip
-            </label>
-            <input type="text" class="form-control" id="inputZip" />
-          </div>
-          <div class="col-12">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck" />
-              <label class="form-check-label" for="gridCheck">
-                Check me out
+    <div className="row m-5">
+      <div className="col-md-6">
+        <div>
+          <form class="row g-3">
+            <div class="col-md-3">
+              <label for="inputEmail4" class="form-label">
+                Email
               </label>
+              <input type="email" class="form-control" id="inputEmail4" />
             </div>
-          </div>
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">
-              Sign in
-            </button>
-          </div>
-        </form>
+            <br />
+            {/* <div class="col-md-3">
+              <label for="inputPassword4" class="form-label">
+                Password
+              </label>
+              <input type="password" class="form-control" id="inputPassword4" />
+            </div> */}
+            <div class="col-4">
+              <label for="inputAddress" class="form-label">
+                Address
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St"
+              />
+            </div>
+            <br />
+            {/* <div class="col-6">
+              <label for="inputAddress2" class="form-label">
+                Address 2
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress2"
+                placeholder="Apartment, studio, or floor"
+              />
+            </div> */}
+            <div class="col-md-6">
+              <label for="inputCity" class="form-label">
+                City
+              </label>
+              <input type="text" class="form-control" id="inputCity" />
+            </div>
+            <br />
+            <div class="col-md-4">
+              <label for="inputState" class="form-label">
+                State
+              </label>
+              <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <br />
+            <div class="col-md-2">
+              <label for="inputZip" class="form-label">
+                Zip
+              </label>
+              <input type="text" class="form-control" id="inputZip" />
+            </div>
+            <br />
+            {/* <div class="col-6">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="gridCheck"
+                />
+                <label class="form-check-label" for="gridCheck">
+                  Check me out
+                </label>
+              </div>
+            </div> */}
+            {/* <div class="col-12">
+              <button type="submit" class="btn btn-primary">
+                Sign in
+              </button>
+            </div> */}
+          </form>
+        </div>
+      </div>
+
+      <div className="col-md-6">
+        <PaymentProcess></PaymentProcess>
       </div>
     </div>
   );
