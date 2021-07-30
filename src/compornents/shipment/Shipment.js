1ba0cadc7307f1ae735f9b68fd6164b1dc4bd57d
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-30 23:09:29 +0600

    lkasdjlajdsf

--- src/compornents/shipment/Shipment.js
+++ src/compornents/shipment/Shipment.js
@@ -23,12 +23,6 @@ const Shipment = () => {
               <input type="email" class="form-control" id="inputEmail4" />
             </div>
             <br />
-            {/* <div class="col-md-3">
-              <label for="inputPassword4" class="form-label">
-                Password
-              </label>
-              <input type="password" class="form-control" id="inputPassword4" />
-            </div> */}
             <div class="col-4">
               <label for="inputAddress" class="form-label">
                 Address
@@ -41,17 +35,7 @@ const Shipment = () => {
               />
             </div>
             <br />
-            {/* <div class="col-6">
-              <label for="inputAddress2" class="form-label">
-                Address 2
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                id="inputAddress2"
-                placeholder="Apartment, studio, or floor"
-              />
-            </div> */}
+
             <div class="col-md-6">
               <label for="inputCity" class="form-label">
                 City
@@ -76,23 +60,6 @@ const Shipment = () => {
               <input type="text" class="form-control" id="inputZip" />
             </div>
             <br />
-            {/* <div class="col-6">
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  id="gridCheck"
-                />
-                <label class="form-check-label" for="gridCheck">
-                  Check me out
-                </label>
-              </div>
-            </div> */}
-            {/* <div class="col-12">
-              <button type="submit" class="btn btn-primary">
-                Sign in
-              </button>
-            </div> */}
           </form>
         </div>
       </div>
